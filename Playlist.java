2b838750ab9459cca038f8f398294fc66264e7c6
unknown <arturodiazmay@gmail.com>
2023-01-23 05:39:49 -0600

    Created Playlist.java + Created Playlist ArrayList + Checked Size and Reduced To 5 or Less

---
+++ Playlist.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class Playlist {
+  
+  public static void main(String[] args) {
+    ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
+    desertIslandPlaylist.add("Frontlines");
+    desertIslandPlaylist.add("Miss you");
+    desertIslandPlaylist.add("Jugaste Y Sufri");
+    desertIslandPlaylist.add("Ecstasy of Soul");
+    desertIslandPlaylist.add("Alive");
+    desertIslandPlaylist.add("Yesterday");
+    desertIslandPlaylist.add("S.O.S.");
+    desertIslandPlaylist.add("Almost Back");
+
+    System.out.println(desertIslandPlaylist);
+    System.out.println(desertIslandPlaylist.size());
+    desertIslandPlaylist.remove("S.O.S.");
+    desertIslandPlaylist.remove("Miss You");
+    desertIslandPlaylist.remove("Yesterday");
+  }  
+}
